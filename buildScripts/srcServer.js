197804ccc5264a590d2d7bedc42db6951c4d4213
Guowei Lv <guowei@tuxera.com>
2018-06-06 16:40:42 +0300

    update

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstname": "Bob", "lastname": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstname": "Tammy", "lastname": "Norton", "email": "tammy@gmail.com"},
+    {"id": 3, "firstname": "Tinna", "lastname": "Lee", "email": "tina@yahoo.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
